b517a13d9f988f093ec1007d454b22fb47baa098
Raphael REME <raphael.reme@orange.fr>
2018-11-27 14:31:18 +0100

    Create the class Media and add a gitignore

---
+++ Media.h
@@ -0,0 +1,36 @@
+#ifndef MEDIA_H
+#define MEDIA_H
+
+#include <string>
+#include <iostream>
+
+
+class Media {
+private:
+  std::string name;
+  std::string file;
+public:
+  Media() {}
+  Media(std::string name, std::string file) : name(name), file(file) {}
+  ~Media() {}
+
+  std::string getName() {
+    return name;
+  }
+
+  std::string getFile() {
+    return file;
+  }
+
+  void setName(std::string name) {
+    this->name = name;
+  }
+
+  void setFile(std::string file) {
+    this->file = file;
+  }
+
+
+};
+
+#endif
