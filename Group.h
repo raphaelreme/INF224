ba880288cec78cdd5e609fa134d2a1ed67973816
Raphael REME <raphael.reme@telecom-paristech.fr>
2018-12-11 16:34:05 +0100

    debut group

---
+++ Group.h
@@ -0,0 +1,21 @@
+#ifndef GROUP_H
+#define GROUP_H
+
+#include <list>
+#include "Media.h"
+
+
+class Group : public std::list<Media> {
+private:
+  std::string name;
+public:
+
+
+
+
+}
+
+
+
+
+#endif GROUP_H
