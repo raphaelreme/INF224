023b2c53d2fd35196f2c2058193b71570ed4d167
Raphael REME <raphael.reme@orange.fr>
2019-02-04 15:08:26 +0100

    Fin etape 1 progression dans l'etape 2

--- interface_java/src/MainFrame.java
+++ interface_java/src/MainFrame.java
@@ -4,12 +4,22 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class MainFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	
+	public static void main(String argv[]) {
+		JFrame frame = new MainFrame();
+		
+	}
+	
 	
 	private JTextArea textArea;
 	private JButton button1;
@@ -17,16 +27,32 @@ public class MainFrame extends JFrame {
 	private JButton button3;
 
 	public MainFrame(){
-		this.setTitle("Medias");
-		//this.setIconImage();
-		//this.setSize(700,700);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		//contentPanel = new Panel(this);
+		//this.setIconImage();
+		initPanel();
+		initMenuBar();
+		
+	
 		//menuBar = new MenuBar(this);
 		
+		
+		
+		//this.setJMenuBar(menuBar);
+		//this.setContentPane(contentPanel);
+		
+		this.setTitle("Medias");
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+	}
+	
+	private void initPanel() {
 		this.textArea = new JTextArea(20,20);
+		
+		JScrollPane scrollPane = new JScrollPane(this.textArea);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		
 		JPanel panel = new JPanel();
 		button1 = new JButton("1");
@@ -60,19 +86,20 @@ public class MainFrame extends JFrame {
 		panel.add(button3);
 		
 		
-		add(this.textArea, BorderLayout.CENTER);
+		add(scrollPane, BorderLayout.CENTER);
 		add(panel, BorderLayout.SOUTH);
-		
-		//this.setJMenuBar(menuBar);
-		//this.setContentPane(contentPanel);
-		this.pack();
-		this.setVisible(true);
-		
-		
 	}
 	
-	public static void main(String argv[]) {
-		JFrame frame = new MainFrame();
+	private void initMenuBar() {
+		JMenuBar bar = new JMenuBar();
+		JMenu menu = new JMenu("Menu 1");
+		JMenuItem item = new JMenuItem("Item 1");
+		
+		menu.add(item);
+		bar.add(menu);
+		setJMenuBar(bar);
+		
 		
 	}
+
 }
